27895a0881701e72a759ce291b01de721770435e
Stevenmindiolaza <sr.mindiolaza@gmail.com>
2021-08-17 14:00:41 -0500

    Primer Commit

---
+++ arr_productos.php
@@ -0,0 +1,24 @@
+<?php
+$productos = array(	
+	0 => array('Nombre' => 'Seleccionar producto',
+				   'Precio' => '80.50'),
+	1 => array('Nombre' => 'computadora',
+				   'Precio' => '1200'),
+
+	2 => array('Nombre' => 'moto',
+				   'Precio' => '900'),
+
+	3 => array('Nombre' => 'cocina',
+				   'Precio' => '400'),
+
+	4 => array('Nombre' => 'mueble',
+				   'Precio' => '500'),
+    
+        
+   
+    
+                                   
+    
+);
+
+?>
